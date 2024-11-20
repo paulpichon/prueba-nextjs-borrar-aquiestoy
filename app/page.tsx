f8a88f3a7da330b55307c272a1af6143340209bd
paul pichon <paul10_barca@hotmail.com>
2024-11-19 20:18:53 -0600

    feat: creacion proyecto borrar nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+
+      <h1>PRUEBA</h1>
       <main className={styles.main}>
         <Image
           className={styles.logo}
